8cdc136c550287daf4f0d3416e8b07632b2c2be4
Cristi Rusu <59122086+cristirusu-99@users.noreply.github.com>
2021-10-26 22:46:18 +0300

    Added basic implementation for Backoffice Component

---
+++ backoffice/Models/RpaDatabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BackofficeComponent.Models
+{
+    public class RpaDatabaseSettings : IRpaDatabaseSettings
+    {
+        public String ProjectsCollectionName { get; set; }
+        public String ConnectionString { get; set; }
+        public String DatabaseName { get; set; }
+    }
+    
+    public interface IRpaDatabaseSettings
+    {
+        String ProjectsCollectionName { get; set; }
+        String ConnectionString { get; set; }
+        String DatabaseName { get; set; }
+    }
+}
---
+++ backoffice/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackofficeComponent.Models;
+using BackofficeComponent.Repositories;
+using Microsoft.Extensions.Options;
+
+namespace BackofficeComponent
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<RpaDatabaseSettings>(
+            Configuration.GetSection(nameof(RpaDatabaseSettings)));
+
+            services.AddSingleton<IRpaDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<RpaDatabaseSettings>>().Value);
+
+            services.AddSingleton<ProjectJsonRepository>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BackofficeComponent", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BackofficeComponent v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
