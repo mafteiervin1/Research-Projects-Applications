0367756f0427468a2614382ed95abc83d82fb4d3
emaftei <ervin.maftei@endava.com>
2021-12-06 16:22:50 +0200

    RPA-31 added BO call from semantic service

--- semanticanalysis/src/main/java/com/seman/service/SemanticWebService.java
+++ semanticanalysis/src/main/java/com/seman/service/SemanticWebService.java
@@ -4,9 +4,10 @@ import com.seman.projhandle.ProjectDetails;
 import com.seman.projhandle.ProjectHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,14 +20,21 @@ public class SemanticWebService {
     @Autowired
     ProjectHandler projectHandler;
 
+    @Value("${backoffice.url:localhost:8090/projectjson}")
+    private String backofficeUrl;
+
     public void handleProjectInformations(List <String> projects) {
         List <ProjectDetails> processedProjectsList = new ArrayList <>();
         projects.forEach(project -> processedProjectsList.add(projectHandler.getProjectDetails(project)));
         persistProjectDetailsToBackoffice(processedProjectsList);
     }
 
-    public ResponseEntity <HttpStatus> persistProjectDetailsToBackoffice(List <ProjectDetails> projectsList) {
-        //httpCall to BO with List of
+    private ResponseEntity <HttpStatus> persistProjectDetailsToBackoffice(List <ProjectDetails> projectsList) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(backofficeUrl, new HttpEntity <>(projectsList.get(0)), String.class );
         return ResponseEntity.ok(OK);
     }
 }
